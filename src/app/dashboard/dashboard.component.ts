31753da519578b108c0f23908ba00cb482df76d0
Fabricio de Paula Oliveira <Fabricio.Oliveira@smanuvemlive.onmicrosoft.com>
2018-05-01 16:09:20 -0300

    Adding dashboard files and configuring the routing module, services, etc

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'lol-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  heroes: Hero[]=[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes() : void {
+    this.heroService.getHeroes()
+      .subscribe(h => this.heroes = h.slice(1,5));
+  }
+
+}
